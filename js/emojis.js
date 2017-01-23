9bea7473eaa34dc4b8a0f1fda06e55f6c7a85d5f
livrofilia <livrofilia@hotmail.com>
2017-01-23 20:31:54 -0300

    *

--- js/emojis.js
+++ js/emojis.js
@@ -1,4 +1,4 @@
-var pre = "<img class='lf-emoticon' src='http://acessoriosparablogs.com.br/emoticons/",
+var pre = "<img class='lf-emoticon' src='http://livrofilia.github.io/img/emojis/",
     pos = ".png' style='border:none' border='0' />",
     PALETA = pre + '002' + pos,
     SOM = pre + '003' + pos,
