124de6bdc7a5006721df26755315379d9bd99d1d
Livrofilia Developers <livrofilia@hotmail.com>
2017-02-03 18:01:52 -0300

    *

---
+++ js/spoiler_alert.js
@@ -0,0 +1,84 @@
+var SPOILER = "\
+<style>\
+.cont {\
+  width: 650px;\
+  max-width: 90%;\
+    height: 800px;\
+    font-size: 12pt;\
+    border: 1px solid #ccc;\
+    border-radius: 5px;\
+    padding: 15px;\
+    color: #636363;\
+    background: #FFFFFF;\
+    margin: 0;\
+}\
+\
+.spoiler {\
+width: calc(100% - 30px);\
+    width: -webkit-calc(100% - 30px);\
+    width: -moz-calc(100% - 30px);\
+    width: -o-calc(100% - 30px);\
+    width: -ms-calc(100% - 30px);\
+    height: auto;\
+    display: inline-block;\
+    font-size: 12pt;\
+    box-shadow: 0 0 20px #000;\
+    -webkit-box-shadow: 0 0 20px #000;\
+    -moz-box-shadow: 0 0 20px #000;\
+    -ms-box-shadow: 0 0 20px #000;\
+    -o-box-shadow: 0 0 20px #000;\
+    border-radius: 5px;\
+    padding: 15px;\
+    background: #FFFDB3;\
+    text-align: justify;\
+    margin: 0;\
+    }\
+\
+.spoiler .icon {\
+  width: 150px;\
+    height: 150px;\
+    display: inline-block;\
+    position: relative;\
+    background-image: url(http://jvribeiro.github.io/spoiler_alert.png);\
+    background-size: 100%;\
+    background-repeat: no-repeat;\
+    background-position: 50%;\
+}\
+.spoiler .text {\
+  color: #000;\
+    font-size: 13pt;\
+    width: 70%;\
+    min-width: 230px;\
+    height: auto;\
+    padding-left: 15px;\
+    padding-right: 15px;\
+    display: inline-block;\
+    top: 0;\
+    left: 170px;\
+}\
+.spoiler .red {\
+  color: #f00;\
+  font-weight: bold;\
+}\
+.spoiler .h2 {\
+  font-size: 20pt;\
+  color: #f00;\
+  text-align: center;\
+  font-weight: bold;\
+}\
+</style>\
+\
+<div class=\"spoiler\">\
+   <div class=\"icon\"><\/div>\
+   <div class=\"text\">\
+     <div class=\"h2\">Alerta de Spoiler!<\/div>\
+     <p>Atenção! Este post contém alguns <a href=\"http://www.livrofilia.com\/p\/politica-de-spoilers.html\" title=\"Política de Spoilers\"><b>SPOILERS<\/b><\/a>! Caso ainda não tenha lido o livro em questão e não quer revelações do enredo, <span class=\"red\">NÃO CONTINUE LENDO<\/span>.<\/p>\
+     <p>Se você se sente confortável com spoilers, <b>prossiga por sua conta e risco<\/b>. Veja nossa <a href=\"http://www.livrofilia.com\/p\/politica-de-spoilers.html\" title=\"Política de Spoilers\"><b>Política de Spoilers<\/b><\/a> para mais informações.<\/p>\
+   <\/div>\
+ <\/div>\
+",
+
+(function ( window )
+{
+  document.body.innerHTML = document.body.innerHTML.replace(/\[\[spoiler\]\]/gi, SPOILER);
+})( window );
